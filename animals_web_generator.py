758b67ef2a5e6b02b6e69ac40d5e87d6ca7976fb
Gabo <no@email>
2024-12-16 10:50:47 +0100

    html serializing structure was actualized | docstrings added

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,6 +1,8 @@
 """
+    Obtaining animal types and information by giving an animal name
 """
 from data_fetcher import fetch_data
+
 
 def load_html_template(file_path):
   """ Loads the html template file """
@@ -8,6 +10,33 @@ def load_html_template(file_path):
       text_data = html_file.read()
       return text_data
 template_html = load_html_template('animals_template.html')
+
+
+def serialize_info_animals(list_animal_types):
+    """ Serializes data to be written in html format. Return de html in a variable 'output'"""
+    output = str()
+    for animal_type in list_animal_types:
+        print(animal_type)
+        output += f'<li class="cards__item">\n'
+        output += f'\t<div class="card__title">{animal_type["name"]}</div>\n'
+
+        output += f'\t<h3>Locations:</h3>\n'
+        output += f'\t<ul class="card__text info_list">\n'
+        locations = animal_type['locations']
+        for location in locations:
+            output += f'\t\t<li>{location}</li>\n'
+        output += f'\t</ul>\n'
+
+        output += f'\t<h3>Characteristics:</h3>\n'
+        output += f'\t<ul class="card__text info_list">\n'
+        characteristics = animal_type['characteristics']
+        for key, value in characteristics.items():
+            output += f'\t\t<li><strong>{key}:</strong> {value}</li>\n'
+        output += f'\t</ul>\n'
+        output += f'</li>\n\n'
+
+    return output
+
 
 def write_new_html(file_path, html_data):
     """ Write a html file replacing a html string with html serialized data """
@@ -18,9 +47,16 @@ def write_new_html(file_path, html_data):
 
 
 def main():
+    """ The user enters an animal name in the terminal.
+        The animal information is obtained from the API.
+        The obtained data is serialized to write it in html.
+        A html file is generated to display the data in a browser.
+    """
     animal_name = input('Enter a name of an animal: ')
-    animals_data = fetch_data(animal_name)
-    print(animals_data)
+    list_animal_types = fetch_data(animal_name)
+    html_data = serialize_info_animals(list_animal_types)
+    write_new_html('animals.html', html_data)
+
 
 if __name__ == '__main__':
     main()
--- data_fetcher.py
+++ data_fetcher.py
@@ -5,11 +5,13 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv() #loads variables from the .env file into the environment
-
 API_KEY = os.getenv('API_KEY') # os.getenv() to access the environment variables loaded from the .env file
 
 
 def fetch_data(animal_name):
+    """ Receives an 'animal name' from the user as an argument.
+        Gets the animal information from the API by request GET.
+        If the response is 'OK' returns the animal infos as json data, if not, prints an error in the terminal """
     API_URL = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
     response = requests.get(API_URL, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
@@ -19,12 +21,9 @@ def fetch_data(animal_name):
         print("Error:", response.status_code, response.text)
 
 
-
 def main():
     pass
     
     
 if __name__ == '__main__':
     main()
-
-
